4912e2be138a4526186273cff263419c1c3187ae
Sohel Ahmed Ansari <sohel.ahmed.ansari@gmail.com>
2025-11-24 10:37:45 +0400

    build: open browser when dev mode is run

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   base: process.env.BASE_URL || "/",
   plugins: [react(), tailwindcss()],
+  server: {
+    open: true,
+  },
 });
